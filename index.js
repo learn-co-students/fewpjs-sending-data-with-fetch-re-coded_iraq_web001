0c9904797e876123619e9c05686f290258196342
Evan hameed <evanhameed9966@gmail.com>
2020-06-01 11:32:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+
+let body = document.getElementsByTagName("body")[0];
+function submitData(name,email){
+
+let data = {
+  name:name,
+  email:email
+};
+
+let configurationObject = {
+
+  method : 'POST',
+  headers : { 'content-type':'application/json' , 'Accept':'application/json'},
+  body : JSON.stringify(data)
+
+}
+return fetch ('http://localhost:3000/users',configurationObject)
+.then(resp=> resp.json())
+.then(resp => {
+
+  body.innerHTML = `${resp.id}`;
+}).catch( error=> {
+
+body.innerHTML = `${error.message}`;
+})
+
+
+
+}
