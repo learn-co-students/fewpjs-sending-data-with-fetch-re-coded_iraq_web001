c360fe088c0c2d95cd1431974faf041239664299
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-05 20:32:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+const body = document.getElementsByTagName('body');
+// const name = document.getElementById('dogName').value;
+// const email = document.getElementById('dogBreed').value;
+
+function submitData(usersName, usersEmail){
+  let formData = {
+    name: usersName,
+    email: usersEmail
+};
+ 
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.innerHTML = object['id'];
+  })
+  .catch(function(error) {
+    document.body.innerHTML = error.message;
+  });
+
+}
