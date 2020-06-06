4a0026bfee8d902560eebb8541065006a3b3b5eb
Zainab Saud <zai99nb@gmail.com>
2020-06-06 09:40:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+let data = {
+name : "Steve",
+email : "steve@steve.com"
+};
+let rando = Math.ceil( Math.random() * 1000 );
+let config = {
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json",
+    "Accept":"application/json"
+    },
+    body : JSON.stringify(data)
+}
+function submitData(name,email){
+return fetch('http://localhost:3000/users',config)
+.then(res=>res.json())
+.then(object=>
+document.body.innerHTML =(object["id"])
+)
+.catch(function(error){
+ document.body.innerHTML=(error.message)
+});
+}
+
